32b6f258feb63b03e87c25201ccf6fb65e06b5f0
scphgithub <scph.mail@gmail.com>
2016-11-04 18:04:46 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -37,6 +37,12 @@ if (!is_null($events['events'])) {
 			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
 			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
 			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			
+			$proxy = 'http://203.157.144.10/';
+			$proxyauth = '';
+			curl_setopt($ch, CURLOPT_PROXY, $proxy);
+			curl_setopt($ch, CURLOPT_PROXYUSERPWD, $proxyauth);
+			
 			$result = curl_exec($ch);
 			curl_close($ch);
 
