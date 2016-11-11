62e48d0de4bb9e5c0c1f604923681a659622a72d
scphgithub <scph.mail@gmail.com>
2016-11-11 15:29:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -6,6 +6,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
+var_dump($content);
 if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
