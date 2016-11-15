621b7fddeb0fd5ac9e9ea2d931d176fac883d91a
scphgithub <scph.mail@gmail.com>
2016-11-15 15:48:49 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,7 +20,7 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => var_dump($events)//$text
+				'text' => "'".var_dump($events)."'"//$text
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
