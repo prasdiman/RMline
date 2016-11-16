7a18241a72e595dd5e4eb4c65e1840d337baeac6
scphgithub <scph.mail@gmail.com>
2016-11-16 14:22:08 +0700

    Update bot.php

--- bot.php
+++ bot.php
@@ -20,13 +20,13 @@ if (!is_null($events['events'])) {
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $content//$text
+				'text' => $text//$content//
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
 			$url = 'https://api.line.me/v2/bot/message/push';
 			$data = [
-				'to' => '962709d6a1918d410be65fe2c053207a',//$replyToken,
+				'replyToken' => $replyToken,
 				'messages' => [$messages],
 			];
 			$post = json_encode($data);
