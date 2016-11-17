dca03435253d03bed1758deea4611d480f928aa0
scphgithub <scph.mail@gmail.com>
2016-11-17 16:18:51 +0700

    Update push_bot.php

--- line-bot-sdk-php-master/line-bot-sdk-tiny/push_bot.php
+++ line-bot-sdk-php-master/line-bot-sdk-tiny/push_bot.php
@@ -16,7 +16,11 @@
  * under the License.
  */
 
-require_once('./LINEBotTiny.php');
+namespace LINE\Tests\LINEBot;
+use LINE\LINEBot;
+use LINE\LINEBot\Constant\MessageType;
+use LINE\LINEBot\MessageBuilder\TextMessageBuilder;
+use LINE\Tests\LINEBot\Util\DummyHttpClient;
 
 $channelAccessToken = 'N4bRWnBZULx0jxZ/nT8S/Run1T10NJpvo9cghxwErdYvqVnLzosaw30CVKCepn4kX9Ad97gSEbb4lb9cKhVeZHMlHAYdTBROCRPcTvfHuih5sIRHlMibUktv3V1tP9Zbq3rCaDr51zgdqO19jTK4+AdB04t89/1O/w1cDnyilFU=';
 $channelSecret = '962709d6a1918d410be65fe2c053207a';
@@ -24,32 +28,32 @@ $channelSecret = '962709d6a1918d410be65fe2c053207a';
 $client = new LINEBotTiny($channelAccessToken, $channelSecret);
 //foreach ($client->parseEvents() as $event) {
 
-/*{
-  "to":["u5912407b444e54885d00111f7b0ce375"],
-  "toChannel":1383378250,
-  "eventType":"138311608800106203",
-  "content":{
-    "contentType":1,
-    "toType":1,
-    "text":"Hello, Jose!"
-  }
-}*/
+/*$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($channelAccessToken);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
 
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('helloTest');
+$response = $bot->pushMessage('ub6d94f94503468c01d1cc9bf40c421f3', $textMessageBuilder);
 
-            $message = [
-							"type": "text",
-							"text": "Hello, world"
-						];
-                    $client->pushMessage(array(
-                        'to' => 'ub6d94f94503468c01d1cc9bf40c421f3',
-			'toChannel' => '1486505354'
-                        'messages' => array(
-                            array(
-                                'type' => 'text',
-                                'text' => $message['text']
-                            )
-                        )
-                    ));
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();*/
 
-echo 'Push-ok';
+$mock = function ($testRunner, $httpMethod, $url, $data) {
+            /** @var \PHPUnit_Framework_TestCase $testRunner */
+            $testRunner->assertEquals('POST', $httpMethod);
+            $testRunner->assertEquals('https://api.line.me/v2/bot/message/push', $url);
+            $testRunner->assertEquals('DESTINATION', $data['to']);
+            $testRunner->assertEquals(3, count($data['messages']));
+            $testRunner->assertEquals(MessageType::TEXT, $data['messages'][0]['type']);
+            $testRunner->assertEquals('test text1', $data['messages'][0]['text']);
+            $testRunner->assertEquals(MessageType::TEXT, $data['messages'][1]['type']);
+            $testRunner->assertEquals('test text2', $data['messages'][1]['text']);
+            $testRunner->assertEquals(MessageType::TEXT, $data['messages'][2]['type']);
+            $testRunner->assertEquals('test text3', $data['messages'][2]['text']);
+            return ['status' => 200];
+        };
+        $bot = new LINEBot(new DummyHttpClient($this, $mock), ['channelSecret' => $channelSecret]);
+        $res = $bot->pushMessage('DESTINATION', new TextMessageBuilder('test text1', 'test text2', 'test text3'));
+        $this->assertEquals(200, $res->getHTTPStatus());
+        $this->assertTrue($res->isSucceeded());
+        $this->assertEquals(200, $res->getJSONDecodedBody()['status']);
+
 //};
