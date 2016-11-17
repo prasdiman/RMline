31afb567d0d1b57aa988326dcb87144ab0c2fb81
scphgithub <scph.mail@gmail.com>
2016-11-17 11:15:15 +0700

    Update LINEBotTiny.php

--- line-bot-sdk-php-master/line-bot-sdk-tiny/LINEBotTiny.php
+++ line-bot-sdk-php-master/line-bot-sdk-tiny/LINEBotTiny.php
@@ -106,10 +106,10 @@ class LINEBotTiny
         ));
 
         $response = file_get_contents('https://api.line.me/v2/bot/message/reply', false, $context);
-        if (strpos($http_response_header[0], '200') === false) {
+        /*if (strpos($http_response_header[0], '200') === false) {
             http_response_code(500);
             error_log("Request failed: " . $response);
-        }
+        }*/
     }
 
     public function pushMessage($message)
@@ -128,10 +128,10 @@ class LINEBotTiny
         ));
 
         $response = file_get_contents('https://api.line.me/v2/bot/message/push', false, $context);
-        if (strpos($http_response_header[0], '200') === false) {
+        /*if (strpos($http_response_header[0], '200') === false) {
             http_response_code(500);
             error_log("Request failed: " . $response);
-        }
+        }*/
     }
 
     private function sign($body)
