1daa974332308daaac6c21c4dd2aa533391d7142
scphgithub <scph.mail@gmail.com>
2016-11-17 15:40:08 +0700

    Update testBotPush.php

--- line-bot-sdk-php-master/tests/testBotPush.php
+++ line-bot-sdk-php-master/tests/testBotPush.php
@@ -1,9 +1,9 @@
 <?php
-require 'bootstrap.php';
+
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('N4bRWnBZULx0jxZ/nT8S/Run1T10NJpvo9cghxwErdYvqVnLzosaw30CVKCepn4kX9Ad97gSEbb4lb9cKhVeZHMlHAYdTBROCRPcTvfHuih5sIRHlMibUktv3V1tP9Zbq3rCaDr51zgdqO19jTK4+AdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '962709d6a1918d410be65fe2c053207a']);
 
-$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('hello');
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('test hellw');
 $response = $bot->pushMessage('ub6d94f94503468c01d1cc9bf40c421f3', $textMessageBuilder);
 
 echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
