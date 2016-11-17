0d1fcd560d9e83a00fa01f46d8a91d9944571438
scphgithub <scph.mail@gmail.com>
2016-11-17 14:59:19 +0700

    Update testBotPush.php

--- line-bot-sdk-php-master/tests/testBotPush.php
+++ line-bot-sdk-php-master/tests/testBotPush.php
@@ -1,5 +1,5 @@
 <?php
-
+require 'bootstrap.php';
 $httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient('N4bRWnBZULx0jxZ/nT8S/Run1T10NJpvo9cghxwErdYvqVnLzosaw30CVKCepn4kX9Ad97gSEbb4lb9cKhVeZHMlHAYdTBROCRPcTvfHuih5sIRHlMibUktv3V1tP9Zbq3rCaDr51zgdqO19jTK4+AdB04t89/1O/w1cDnyilFU=');
 $bot = new \LINE\LINEBot($httpClient, ['channelSecret' => '962709d6a1918d410be65fe2c053207a']);
 
